c2c158293c563a7b9e9d02afc82a50b2926e3edc
ScrRa <jesus@squad.com.mx>
2013-10-17 23:58:42 -0500

    Udp start

---
+++ UdpClient/Osc/Udp/UdpClient.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.SPOT;
+using System.Net;
+using System.Net.Sockets;
+
+/*
+ * Author: Scr_Ra
+ * Date: October 11th, 2013
+ * Version: 1.0
+ */
+
+namespace Osc.Udp
+{
+    class UdpClient
+    {
+        protected int _port
+    }
+}
